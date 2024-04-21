a464921b0e1180ec7cfa2e9ca4ec485ba6d746c0
Gaúcho <joao_c_caetano@estudante.sesisenai.org.br>
2024-04-21 20:38:21 -0300

    v1.0

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function calcular() {
-    if (soma() = true);
-}
-
+var primeiroNumero = 0;
+var segundoNumero = 0;
+var resultado = 0
 function soma() {
-
+    soma() === true;
+}
+function calcular() {
+    if (soma() === true) {
+        resultado = primeiroNumero + segundoNumero;
+    }
+    alert("seu resultado foi: " + resultado);
 }
